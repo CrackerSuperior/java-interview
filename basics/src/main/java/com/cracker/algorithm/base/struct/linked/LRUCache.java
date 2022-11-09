a6af91997a1411bae076a6ee6e39f271378c8110
Cracker <2278801557@qq.com>
2022-11-09 16:44:43 +0800

    :sparkles: feat: Add basic implementation of LRUCache.

---
+++ basics/src/main/java/com/cracker/algorithm/base/struct/linked/LRUCache.java
@@ -0,0 +1,43 @@
+package com.cracker.algorithm.base.struct.linked;
+
+public class LRUCache<I> extends SinglyLinked<I> {
+    //TODO: After Map is implemented, LRU is fully implemented.
+
+    /**
+     * Default cache size.
+     */
+    private static final int DEFAULT_CAPACITY = 10;
+    
+    private final int capacity;
+
+    public LRUCache() {
+        this.capacity = DEFAULT_CAPACITY;
+    }
+
+    public LRUCache(final int capacity) {
+        this.capacity = capacity;
+    }
+
+    @Override
+    public void add(final I item) {
+        if (isFull()) {
+            removeLast();
+        }
+        addFirst(item);
+    }
+
+    public I get() {
+        return getFirstItem();
+    }
+    
+    @Override
+    public I get(final int key) {
+        I item = remove(key);
+        add(item);
+        return item;
+    }
+    
+    public boolean isFull() {
+        return size() == capacity;
+    }
+}
