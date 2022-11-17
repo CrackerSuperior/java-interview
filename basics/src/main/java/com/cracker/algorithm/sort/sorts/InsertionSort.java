3df7bb1eeee58c6c9e4d2989cc04c9efb4f5a788
Cracker <2278801557@qq.com>
2022-11-17 17:37:33 +0800

    :sparkles: feat: Add insertion sort and unit tests.

---
+++ basics/src/main/java/com/cracker/algorithm/sort/sorts/InsertionSort.java
@@ -0,0 +1,23 @@
+package com.cracker.algorithm.sort.sorts;
+
+import com.cracker.algorithm.sort.core.Sort;
+
+public class InsertionSort<I> extends Sort<I> {
+
+    public InsertionSort() {
+        
+    }
+
+    public InsertionSort(final boolean reverse) {
+        super(reverse);
+    }
+
+    @Override
+    public void sort(final Comparable<I>[] data) {
+        for (int i = 1; i < data.length; i++) {
+            for (int j = i; j > 0 && less(data[j - 1], data[j]); j--) {
+                exch(data, j, j - 1);
+            }
+        }
+    }
+}
--- basics/src/test/java/com/cracker/algorithm/sort/SortTest.java
+++ basics/src/test/java/com/cracker/algorithm/sort/SortTest.java
@@ -1,6 +1,7 @@
 package com.cracker.algorithm.sort;
 
 import com.cracker.algorithm.sort.sorts.BubbleSort;
+import com.cracker.algorithm.sort.sorts.InsertionSort;
 import com.cracker.algorithm.sort.sorts.SelectionSort;
 import org.junit.jupiter.api.Test;
 
@@ -44,6 +45,26 @@ class SortTest {
         int[] index = {0};
         Arrays.stream(integers).forEach(each -> assertEquals(ints[index[0]++], each));
         SelectionSort<Integer> sort1 = new SelectionSort<>(true);
+        assertFalse(sort1.isSorted(integers));
+        sort1.sort(integers);
+        int[] ints2 = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+        index[0] = 0;
+        assertTrue(sort1.isSorted(integers));
+        Arrays.stream(integers).forEach(each -> assertEquals(ints2[index[0]++], each));
+    }
+    
+    @Test
+    void insertionSortTest() {
+        int[] data = {2, 5, 1, 3, 6, 9, 0, 8, 7, 4};
+        InsertionSort<Integer> sort = new InsertionSort<>();
+        Integer[] integers = Arrays.stream(data).boxed().toArray(Integer[]::new);
+        assertFalse(sort.isSorted(integers));
+        sort.sort(integers);
+        assertTrue(sort.isSorted(integers));
+        int[] ints = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] index = {0};
+        Arrays.stream(integers).forEach(each -> assertEquals(ints[index[0]++], each));
+        InsertionSort<Integer> sort1 = new InsertionSort<>(true);
         assertFalse(sort1.isSorted(integers));
         sort1.sort(integers);
         int[] ints2 = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
